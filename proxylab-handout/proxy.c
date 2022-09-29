ae4b65cead1d638e2b635a96fb3d5c459793d62a
eternal <100830314+orientlegend@users.noreply.github.com>
2022-09-29 20:15:40 +0800

    2022-09-29 Finished the part I of proxylab

---
+++ proxylab-handout/proxy.c
@@ -0,0 +1,205 @@
+#include "csapp.h"
+#include "tiny/csapp.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+#define HEADER_MAX_NUM 20
+
+/* You won't lose style points for including this long line in your code */
+static const char *user_agent_hdr =
+    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
+    "Firefox/10.0.3\r\n";
+
+typedef struct {
+  char method[MAXLINE];
+  char host[MAXLINE];
+  char port[MAXLINE];
+  char path[MAXLINE];
+  char version[MAXLINE];
+} request_line;
+
+typedef struct {
+  char name[MAXLINE];  // The name of request header
+  char value[MAXLINE]; // The value of request header
+} request_header;
+
+typedef struct {
+  request_line line;
+  request_header headers[HEADER_MAX_NUM];
+  int header_num; // The number of headers
+} request;
+
+static void doit(int connfd);
+static void parse_url(char *uri, char *host, char *port, char *path);
+static void read_request(int fd, request *request);
+static void read_request_line(rio_t *rp, char *buf, request_line *request_line);
+static void read_request_header(rio_t *rp, char *buf, request_header *header);
+static int send_request(request *request);
+static void forward_response(int connfd, int targetfd);
+
+static void sigpipe_handler(int sig);
+
+int main(int argc, char **argv) {
+  int listenfd, connfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  struct sockaddr_storage clientaddr;
+  socklen_t clientlen;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  Signal(SIGPIPE, sigpipe_handler);
+  listenfd = Open_listenfd(argv[1]);
+
+  while (true) {
+    clientlen = sizeof(struct sockaddr_storage);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
+                0);
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+    doit(connfd);
+    Close(connfd);
+  }
+  exit(0);
+}
+
+static void sigpipe_handler(int sig) {
+  fprintf(stderr, "Receive signal: %d\n", sig);
+  perror("Error: connection reset by peer\n");
+}
+
+static void doit(int connfd) {
+  request request;
+  read_request(connfd, &request); // read the message from client
+  int clientfd =
+      send_request(&request); // establish a new connection with target host
+  forward_response(connfd, clientfd); // forward the response to client
+  Close(clientfd);                    // Close the connection with target host
+}
+
+static void read_request(int fd, request *request) {
+  char buf[MAXLINE];
+  rio_t rio;
+  Rio_readinitb(&rio, fd);
+  read_request_line(&rio, buf, &request->line);
+
+  request_header *header = request->headers;
+  request->header_num = 0;
+  Rio_readlineb(&rio, buf, MAXLINE);
+  while (strcmp(buf, "\r\n")) { // read the headers
+    read_request_header(&rio, buf, header++);
+    request->header_num++;
+    Rio_readlineb(&rio, buf, MAXLINE);
+  }
+}
+
+static void read_request_line(rio_t *rp, char *buf,
+                              request_line *request_line) {
+  char uri[MAXLINE];
+  Rio_readlineb(rp, buf, MAXLINE);
+  printf("Request: %s\n", buf);
+  if (sscanf(buf, "%s %s %s", request_line->method, uri,
+             request_line->version) < 3) {
+    perror("Error: invalid request line\n");
+    exit(1);
+  }
+  if (strcasecmp(request_line->method, "GET")) { // If the method is not GET
+    perror("Error: method not implemented\n");
+    exit(1);
+  }
+  if (strcasecmp(request_line->version, "HTTP/1.0") &&
+      strcasecmp(request_line->version, "HTTP/1.1")) {
+    perror("HTTP version not recognized\n");
+    exit(1);
+  }
+  parse_url(uri, request_line->host, request_line->port, request_line->path);
+}
+
+static void read_request_header(rio_t *rp, char *buf, request_header *header) {
+  Rio_readlineb(rp, buf, MAXLINE);
+  char *c = strstr(buf, ": ");
+  if (c == NULL) {
+    fprintf(stderr, "Error: invalid header: %s\n", buf);
+    exit(1);
+  }
+  *c = '\0';
+  strcpy(header->name, buf);
+  strcpy(header->value, c + 2);
+}
+
+static int send_request(request *request) {
+  int clientfd;
+  char content[MAXLINE];
+  rio_t rio;
+  request_line *request_line = &request->line;
+  clientfd = Open_clientfd(request_line->host, request_line->port);
+  Rio_readinitb(&rio, clientfd);
+  sprintf(content, "%s %s HTTP/1.0\r\n", request_line->method,
+          request_line->path);
+  sprintf(content, "%sHost: %s:%s\r\n", content, request_line->host,
+          request_line->port);
+  sprintf(content, "%s%s\r\n", content, user_agent_hdr);
+  sprintf(content, "%sConnection: close\r\n", content);
+  sprintf(content, "%sProxy-Connection: close\r\n", content);
+  for (int i = 0; i < request->header_num; ++i) {
+    request_header header = request->headers[i];
+    char *name = header.name;
+    char *value = header.value;
+    if (!strcasecmp(name, "Host") || !strcasecmp(name, "User-Agent") ||
+        !strcasecmp(name, "Connection") ||
+        !strcasecmp(name, "Proxy-Connection")) {
+      continue;
+    }
+    sprintf(content, "%s%s: %s\r\n\r\n", content, name, value);
+  }
+  Rio_writen(clientfd, content, strlen(content));
+  return clientfd;
+}
+
+static void forward_response(int connfd, int targetfd) {
+  rio_t rio;
+  int n;
+  char buf[MAXLINE];
+  Rio_readinitb(&rio, targetfd);
+  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) > 0) {
+    Rio_writen(connfd, buf, n);
+  }
+}
+
+static void parse_url(char *uri, char *host, char *port, char *path) {
+  char *hostpose = strstr(uri, "//");
+  if (hostpose == NULL) {
+    char *pathpose = strstr(uri, "/");
+    if (pathpose != NULL) {
+      strcpy(path, pathpose);
+    }
+    strcpy(port, "80"); // The default port is 80
+    return;
+  } else {
+    char *portpose = strstr(hostpose + 2, ":");
+    if (portpose != NULL) {
+      int portnum;
+      sscanf(portpose + 1, "%d%s", &portnum, path);
+      sprintf(port, "%d", portnum);
+      *portpose = '\0';
+    } else {
+      char *pathpose = strstr(hostpose + 2, "/");
+      if (pathpose != NULL) {
+        strcpy(path, pathpose);
+        strcpy(port, "80");
+        *pathpose = '\0';
+      }
+    }
+    strcpy(host, hostpose + 2);
+  }
+  return;
+}
